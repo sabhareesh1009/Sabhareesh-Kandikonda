544f2ffacbc81c751477b9e45695d2eff7f4e404
sabhareesh1009 <sabhareeshkandikonda1109@gmail.com>
2021-12-01 00:25:59 +0530

    Solved the rearrange of positive and negative nums in O(1) space

--- Arrays/geeksForGeeks/Array_reArrangements/rearrangePositiveNegativeOrder.js
+++ Arrays/geeksForGeeks/Array_reArrangements/rearrangePositiveNegativeOrder.js
@@ -1,9 +1,28 @@
 // Rearrange positive and negative numbers in O(n) time and O(1) extra space
 
-
 const reArrangePosNeg = (arr) => {
-    let tempArray = [];
-    for (let i = 0; i < arr.length; i++) {
-    
+  let temp = 0;
+  let ind = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      temp = arr[ind];
+      arr[ind] = arr[i];
+      arr[i] = temp;
+      ind++;
     }
-}
+  }
+
+  let posInd = ind;
+  let negInd = 0;
+  while (negInd < posInd && posInd < arr.length && arr[negInd] < 0) {
+    temp = arr[negInd];
+    arr[negInd] = arr[posInd];
+    arr[posInd] = temp;
+    negInd = negInd + 2;
+    posInd++;
+  }
+  return arr;
+};
+
+let reArr = [-1, 2, -3, 4, 5, 6, -7, 8, 9];
+console.log(reArrangePosNeg(reArr));
